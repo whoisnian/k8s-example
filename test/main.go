a49e30c586ec312e11e5ec3194d4e94e7b223a63
whoisnian <zhuchangbao1998@gmail.com>
2022-04-25 00:37:48 +0800

    ADD: k8s-example-test

---
+++ test/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/whoisnian/glb/httpd"
+	"github.com/whoisnian/glb/logger"
+)
+
+var (
+	listenAddr = "0.0.0.0:8080"
+
+	MiB = bytes.Repeat([]byte("0123456789abcdef"), 65536)
+)
+
+func pingHandler(store *httpd.Store) {
+	logger.Debug("Recerved ping request.")
+	store.Respond200([]byte("pong\n"))
+}
+
+func memHandler(store *httpd.Store) {
+	cnt, err := strconv.Atoi(store.R.FormValue("cnt"))
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	buf := [][]byte{}
+	for i := 0; i < cnt; i++ {
+		tmp := make([]byte, len(MiB))
+		copy(tmp, MiB)
+		buf = append(buf, tmp)
+	}
+
+	time.Sleep(time.Second * 10)
+	res := strconv.Itoa(len(buf)) + " MiB\n"
+	store.Respond200([]byte(res))
+}
+
+func main() {
+	logger.SetDebug(true)
+
+	mux := httpd.NewMux()
+	mux.Handle("/ping", "GET", pingHandler)
+	mux.Handle("/mem", "GET", memHandler)
+
+	go func() {
+		logger.Info("Service started. (", os.Getpid(), ")")
+		if err := http.ListenAndServe(listenAddr, logger.Req(logger.Recovery(mux))); err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	done := make(chan struct{})
+	once := sync.Once{}
+	go func() {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		for rec := range ch {
+			logger.Warn("Signal received: ", rec)
+			once.Do(func() { close(done) })
+		}
+	}()
+
+	<-done
+	time.Sleep(time.Second * 20)
+	logger.Info("Service stopped.")
+}
