4619ec4db79d836d77ff8c28ba845af430f0877a
whoisnian <zhuchangbao1998@gmail.com>
2022-04-28 21:28:44 +0800

    ADD: multiple containers pod

--- test/main.go
+++ test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,14 +16,40 @@ import (
 )
 
 var (
+	// config env
 	listenAddr = "0.0.0.0:8080"
+	upstream   = ""
+	modeFile   = "./mode"
 
+	// runtime flag
+	modeList   = []string{"normal", "reject", "panic"}
+	serverMode = "normal"
+
+	// constant
 	MiB = bytes.Repeat([]byte("0123456789abcdef"), 65536)
 )
 
 func init() {
 	if val, ok := os.LookupEnv("LISTEN_ADDR"); ok {
 		listenAddr = val
+	}
+	if val, ok := os.LookupEnv("UPSTREAM"); ok {
+		upstream = val
+	}
+	if val, ok := os.LookupEnv("MODE_FILE"); ok {
+		modeFile = val
+	}
+	if f, err := os.Open(modeFile); err == nil {
+		defer f.Close()
+		if fbuf, err := io.ReadAll(f); err == nil {
+			fstr := string(bytes.TrimSpace(fbuf))
+			for _, mode := range modeList {
+				if fstr == mode {
+					serverMode = mode
+					break
+				}
+			}
+		}
 	}
 }
 
@@ -50,19 +77,39 @@ func memHandler(store *httpd.Store) {
 	store.Respond200([]byte(res))
 }
 
+func upstreamHandler(store *httpd.Store) {
+	logger.Debug("Received upstream request.")
+	if upstream == "" {
+		store.Respond200([]byte("success\n"))
+	} else {
+		resp, err := http.Get(upstream)
+		if err != nil {
+			logger.Panic(err)
+		}
+		defer resp.Body.Close()
+		io.Copy(store.W, resp.Body)
+	}
+}
+
 func main() {
 	logger.SetDebug(true)
+	if serverMode == "panic" {
+		logger.Panic("panic mode")
+	}
 
 	mux := httpd.NewMux()
 	mux.Handle("/ping", "GET", pingHandler)
 	mux.Handle("/mem", "GET", memHandler)
+	mux.Handle("/upstream", "GET", upstreamHandler)
 
-	go func() {
-		logger.Info("Service started. (", os.Getpid(), ")")
-		if err := http.ListenAndServe(listenAddr, logger.Req(logger.Recovery(mux))); err != nil {
-			logger.Fatal(err)
-		}
-	}()
+	logger.Info("Service started. (", os.Getpid(), ")")
+	if serverMode != "reject" {
+		go func() {
+			if err := http.ListenAndServe(listenAddr, logger.Req(logger.Recovery(mux))); err != nil {
+				logger.Fatal(err)
+			}
+		}()
+	}
 
 	done := make(chan struct{})
 	once := sync.Once{}
