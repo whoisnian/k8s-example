37bda78e0fda8dc6f03e0b5f06ab8b992034aa67
whoisnian <zhuchangbao1998@gmail.com>
2024-04-14 17:13:01 +0800

    ADD: gin debug print with zap

--- src/file/global/logger.go
+++ src/file/global/logger.go
@@ -2,6 +2,7 @@ package global
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var LOG *zap.Logger
@@ -10,21 +11,49 @@ func SetupLogger() {
 	var err error
 	if CFG.Debug {
 		LOG, err = zap.Config{
-			Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
-			Development:      true,
-			Encoding:         "console",
-			EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
-			OutputPaths:      []string{"stderr"},
-			ErrorOutputPaths: []string{"stderr"},
+			Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+			Development: true,
+			Encoding:    "console",
+			EncoderConfig: zapcore.EncoderConfig{ // from zap.NewDevelopmentEncoderConfig()
+				TimeKey:        "T",
+				LevelKey:       "L",
+				NameKey:        "N",
+				CallerKey:      "C",
+				FunctionKey:    zapcore.OmitKey,
+				MessageKey:     "M",
+				StacktraceKey:  "S",
+				LineEnding:     zapcore.DefaultLineEnding,
+				EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+				EncodeTime:     zapcore.ISO8601TimeEncoder,
+				EncodeDuration: zapcore.StringDurationEncoder,
+				EncodeCaller:   zapcore.ShortCallerEncoder,
+			},
+			DisableStacktrace: true,
+			OutputPaths:       []string{"stderr"},
+			ErrorOutputPaths:  []string{"stderr"},
 		}.Build()
 	} else {
 		LOG, err = zap.Config{
-			Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
-			Development:      false,
-			Encoding:         "json",
-			EncoderConfig:    zap.NewProductionEncoderConfig(),
-			OutputPaths:      []string{"stderr"},
-			ErrorOutputPaths: []string{"stderr"},
+			Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+			Development: false,
+			Encoding:    "json",
+			EncoderConfig: zapcore.EncoderConfig{ // from zap.NewProductionEncoderConfig()
+				TimeKey:        "ts",
+				LevelKey:       "level",
+				NameKey:        "logger",
+				CallerKey:      "caller",
+				FunctionKey:    zapcore.OmitKey,
+				MessageKey:     "msg",
+				StacktraceKey:  "stacktrace",
+				LineEnding:     zapcore.DefaultLineEnding,
+				EncodeLevel:    zapcore.LowercaseLevelEncoder,
+				EncodeTime:     zapcore.EpochTimeEncoder,
+				EncodeDuration: zapcore.MillisDurationEncoder,
+				EncodeCaller:   zapcore.ShortCallerEncoder,
+			},
+			DisableStacktrace: true,
+			OutputPaths:       []string{"stderr"},
+			ErrorOutputPaths:  []string{"stderr"},
 		}.Build()
 	}
 	if err != nil {
--- src/file/go.mod
+++ src/file/go.mod
@@ -3,7 +3,7 @@ module github.com/whoisnian/k8s-example/src/file
 go 1.22.2
 
 require (
-	github.com/gin-gonic/gin v1.9.1
+	github.com/gin-gonic/gin v1.9.2-0.20240402105722-c6f90df4e0c8
 	go.opentelemetry.io/otel/trace v1.25.0
 	go.uber.org/zap v1.27.0
 )
@@ -25,7 +25,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
