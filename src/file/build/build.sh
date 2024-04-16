871420c00313a8cd52df5162e236af8e9221fa8f
whoisnian <zhuchangbao1998@gmail.com>
2024-04-17 02:10:34 +0800

    ADD: k8s-example-file dockerfile & build script

---
+++ src/file/build/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+#########################################################################
+# File Name: build.sh
+# Author: nian
+# Blog: https://whoisnian.com
+# Mail: zhuchangbao1998@gmail.com
+# Created Time: 2024年02月10日 星期六 20时10分28秒
+#########################################################################
+
+SCRIPT_DIR=$(dirname "$0")
+SOURCE_DIR="$SCRIPT_DIR/.."
+OUTPUT_DIR="$SOURCE_DIR/output"
+
+MODULE_NAME=$(go mod edit -fmt -print | grep -Po '(?<=^module ).*$')
+APP_NAME="k8s-example-file"
+BUILDTIME=$(date --iso-8601=seconds)
+if [[ -z "$GITHUB_REF_NAME" ]]; then
+  VERSION=$(git describe --tags 2> /dev/null || echo unknown)
+else
+  VERSION=$GITHUB_REF_NAME
+fi
+
+goBuild() {
+  CGO_ENABLED=0 GOOS="$1" GOARCH="$2" go build -trimpath \
+    -ldflags="-s -w \
+    -X '${MODULE_NAME}/global.AppName=${APP_NAME}' \
+    -X '${MODULE_NAME}/global.Version=${VERSION}' \
+    -X '${MODULE_NAME}/global.BuildTime=${BUILDTIME}'" \
+    -o "$OUTPUT_DIR"/"$3" "$SOURCE_DIR"
+}
+
+if [[ "$1" == '.' ]]; then
+  goBuild $(go env GOOS) $(go env GOARCH) "$APP_NAME"
+elif [[ "$1" == 'all' ]]; then
+  goBuild linux amd64 "${APP_NAME}-linux-amd64-${VERSION}"
+  goBuild linux arm64 "${APP_NAME}-linux-arm64-${VERSION}"
+elif [[ "$#" == 2 ]]; then
+  goBuild "$1" "$2" "${APP_NAME}-$1-$2-${VERSION}"
+else
+  cat << EOF
+Usage:
+  $(basename $0) .            # build for current platform $(go env GOOS)-$(go env GOARCH)
+  $(basename $0) all          # build for all supported platforms
+  $(basename $0) darwin amd64 # build for specified platform
+
+Supported platforms:
+  linux-amd64
+  linux-arm64
+EOF
+  exit 1
+fi
