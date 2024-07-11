65e5858521e8f2b775ad6e4ccf6dd1b96273a667
whoisnian <zhuchangbao1998@gmail.com>
2024-07-12 00:50:16 +0800

    ADD: build with golang ModName

--- src/user/global/global.go
+++ src/user/global/global.go
@@ -3,6 +3,7 @@ package global
 var (
 	// build-time variables with `go build -ldflags="-X importpath.name=value"`
 	// `-X` option from `go tool link --help`
+	ModName   = "unknown"
 	AppName   = "unknown"
 	Version   = "unknown"
 	BuildTime = "unknown"
