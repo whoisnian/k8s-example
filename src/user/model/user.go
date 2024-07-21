27c15135e8c3df1d1a94cb281df56343db3e379b
whoisnian <zhuchangbao1998@gmail.com>
2024-07-21 19:49:48 +0800

    CHG: separate internal and external models

--- src/user/model/user.go
+++ src/user/model/user.go
@@ -9,13 +9,33 @@ type User struct {
 	ID       int64  `gorm:"column:id;type:bigint(20);primaryKey;autoIncrement:true" json:"id"`
 	Name     string `gorm:"column:name;type:varchar(255)" json:"name"`
 	Email    string `gorm:"column:email;type:varchar(255);index:users_email_idx,unique" json:"email"`
-	Password string `gorm:"column:password;type:varchar(255)" json:"-"`
+	Password string `gorm:"column:password;type:varchar(255)" json:"password"`
 
-	DeletedAt sql.NullTime `gorm:"column:deleted_at;type:datetime;default:null" json:"-"`
+	DeletedAt sql.NullTime `gorm:"column:deleted_at;type:datetime;default:null" json:"deleted_at"`
 	CreatedAt time.Time    `gorm:"column:created_at;type:datetime;autoCreateTime;not null" json:"created_at"`
 	UpdatedAt time.Time    `gorm:"column:updated_at;type:datetime;autoUpdateTime;not null" json:"updated_at"`
 }
 
+type UserJson struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 func (*User) TableName() string {
 	return "users"
+}
+
+func (raw *User) AsJson() UserJson {
+	return UserJson{
+		ID:    raw.ID,
+		Name:  raw.Name,
+		Email: raw.Email,
+
+		CreatedAt: raw.CreatedAt,
+		UpdatedAt: raw.UpdatedAt,
+	}
 }
