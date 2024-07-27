57c94d0a144be4712fd8333f5db18974192feedc
whoisnian <zhuchangbao1998@gmail.com>
2024-07-28 01:37:18 +0800

    CHG: (fe) fileSize as blob json & code clean up

--- src/fe/html/static/view.js
+++ src/fe/html/static/view.js
@@ -5,22 +5,22 @@ import { fetchGetJSON, fetchDeleteHead, fetchGetJSONWithStatus, fetchPostJSONWit
 const createFileItem = ({ id, name, size, created_at }) => {
   const tr = createElement('tr')
 
-  const nameTd = createElement('td', { style: "text-align:left;" })
+  const nameTd = createElement('td', { style: 'text-align:left;' })
   nameTd.textContent = name
 
-  const sizeTd = createElement('td', { style: "text-align:right;" })
+  const sizeTd = createElement('td', { style: 'text-align:right;' })
   sizeTd.textContent = calcFromBytes(size)
 
   const ctime = new Date(created_at)
-  const timeTd = createElement('td', { style: "text-align:right;", title: ctime.toLocaleString() })
+  const timeTd = createElement('td', { style: 'text-align:right;', title: ctime.toLocaleString() })
   timeTd.textContent = calcRelativeTime(ctime)
 
-  const actionTd = createElement('td', { style: "text-align:right;" })
+  const actionTd = createElement('td', { style: 'text-align:right;' })
   const downBtn = createElement('button')
-  downBtn.textContent = "SAVE"
+  downBtn.textContent = 'SAVE'
   downBtn.onclick = () => downloadFile(`/file/objects/${id}`, name)
   const delBtn = createElement('button')
-  delBtn.textContent = "DEL"
+  delBtn.textContent = 'DEL'
   delBtn.onclick = async () => {
     await fetchDeleteHead(`/file/objects/${id}`)
     tr.remove()
@@ -36,13 +36,10 @@ const createFileItem = ({ id, name, size, created_at }) => {
   return tr
 }
 
-const fileSizeInput = document.getElementById('fileSizeInput')
 const fileListInput = document.getElementById('fileListInput')
-
 const fileListButton = document.getElementById('fileListButton')
-fileListButton.onclick = () => fileListInput.click()
-
 const fileListText = document.getElementById('fileListText')
+fileListButton.onclick = () => fileListInput.click()
 fileListText.value = 'No file selected'
 fileListInput.onchange = () => {
   if (fileListInput.files.length == 1) fileListText.value = fileListInput.files.item(0).name
@@ -50,12 +47,17 @@ fileListInput.onchange = () => {
 }
 
 const uploadFormButton = document.getElementById('uploadFormButton')
-uploadFormButton.onclick = () => {
+uploadFormButton.onclick = (e) => {
+  e.preventDefault()
+
+  // https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#multipart-form-data
+	// The order of parts must be the same as the order of fields in entry list.
+  const formData = new FormData()
   const sizes = []
   for (const f of fileListInput.files) sizes.push(f.size)
-  fileSizeInput.value = JSON.stringify(sizes)
+  formData.append('fileSize', new Blob([JSON.stringify(sizes)], { type: 'application/json' }), 'sizes.json')
+  for (const f of fileListInput.files) formData.append('fileList', f, f.name)
 
-  const formData = new FormData(document.getElementById('uploadForm'))
   new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest()
     xhr.withCredentials = true
@@ -82,7 +84,8 @@ signinButton.onclick = () => signinDialog.showModal()
 signinDialog.onclick = dialogCloser(signinDialog)
 
 const signinFormButton = document.getElementById('signinFormButton')
-signinFormButton.onclick = async () => {
+signinFormButton.onclick = async (e) => {
+  e.preventDefault()
   const formData = new FormData(document.getElementById('signinForm'))
   const { ok } = await fetchPostJSONWithStatus('/user/signin', Object.fromEntries(formData.entries()))
   if (ok) reloadPage()
@@ -94,7 +97,8 @@ signupButton.onclick = () => signupDialog.showModal()
 signupDialog.onclick = dialogCloser(signupDialog)
 
 const signupFormButton = document.getElementById('signupFormButton')
-signupFormButton.onclick = async () => {
+signupFormButton.onclick = async (e) => {
+  e.preventDefault()
   const formData = new FormData(document.getElementById('signupForm'))
   const { ok } = await fetchPostJSONWithStatus('/user/signup', Object.fromEntries(formData.entries()))
   if (ok) reloadPage()
