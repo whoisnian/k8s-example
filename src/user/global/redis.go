1fc88210aa4ade2b71f4a2889052ac3f18fec2c7
whoisnian <zhuchangbao1998@gmail.com>
2024-07-28 03:26:20 +0800

    CHG: (user) rbcervilla/redisstore as gin-contrib/sessions backend

--- src/user/global/redis.go
+++ src/user/global/redis.go
@@ -3,10 +3,16 @@ package global
 import (
 	"context"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/rbcervilla/redisstore/v9"
+	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
 )
 
-var RDB *redis.Client
+var (
+	RDB *redis.Client
+	RSS sessions.Store
+)
 
 func SetupRedis() {
 	opts, err := redis.ParseURL(CFG.RedisURI)
@@ -15,7 +21,20 @@ func SetupRedis() {
 	}
 
 	RDB = redis.NewClient(opts)
-	if err = RDB.Ping(context.Background()).Err(); err != nil {
+	if err := redisotel.InstrumentTracing(RDB); err != nil {
 		panic(err)
 	}
+	store, err := redisstore.NewRedisStore(context.Background(), RDB)
+	if err != nil {
+		panic(err)
+	}
+	RSS = SessionsStore{store}
+}
+
+type SessionsStore struct {
+	*redisstore.RedisStore
+}
+
+func (s SessionsStore) Options(options sessions.Options) {
+	s.RedisStore.Options(*options.ToGorillaOptions())
 }
